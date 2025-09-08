3801c2dc9d778b9f54e8065771d18681564c305e
bolgov.n <bolgov.n@selectel.com>
2025-09-08 16:28:13 +0300

    HW-1: refactoring

--- services/pet/pet_api.py
+++ services/pet/pet_api.py
@@ -1,33 +1,29 @@
-import requests
+from services.base import Base
 from services.pet.endpoints import Endpoints
-import json
 
-
-class PetAPI:
+class PetAPI(Base):
     @staticmethod
-    def create_pet(pet_data):
-        response = requests.post(Endpoints.PET, json=pet_data)
-        return response
+    def create_pet(pet_data): 
+        return PetAPI.post(Endpoints.PET, json=pet_data)
 
     @staticmethod
     def update_pet(pet_data):
-        response = requests.put(Endpoints.PET, json=pet_data)
-        return response
+        return PetAPI.put(Endpoints.PET, json=pet_data)
 
     @staticmethod
     def get_pet_by_id(pet_id):
-        return requests.get(Endpoints.PET_ID.format(pet_id))
+        return PetAPI.get(Endpoints.PET_ID.format(pet_id))
 
     @staticmethod
     def delete_pet(pet_id):
-        return requests.delete(Endpoints.PET_ID.format(pet_id))
+        return PetAPI.delete(Endpoints.PET_ID.format(pet_id))
 
     @staticmethod
     def find_pet_by_status(status):
-        return requests.get(Endpoints.FIND_PET_BY_STATUS, params={"status": status})
+        return PetAPI.get(Endpoints.FIND_PET_BY_STATUS, params={"status": status})
 
     @staticmethod
     def upload_image(pet_id, image_path):
         with open(image_path, "rb") as f:
             files = {"file": f}
-            return requests.post(Endpoints.UPLOAD_IMAGE.format(pet_id), files=files)
+            return PetAPI.post(Endpoints.UPLOAD_IMAGE.format(pet_id), files=files)
--- services/store/store_api.py
+++ services/store/store_api.py
@@ -1,20 +1,20 @@
-import requests
+from services.base import Base
 from services.store.endpoints import Endpoints
 
 
-class StoreAPI:
+class StoreAPI(Base):
     @staticmethod
     def create_order(order_data):
-        return requests.post(Endpoints.PLACE_ORDER, json=order_data)
+        return StoreAPI.post(Endpoints.PLACE_ORDER, json=order_data)
 
     @staticmethod
-    def get_order(order_id: int):
-        return requests.get(Endpoints.FIND_ORDER.format(order_id))
+    def get_order(order_id):
+        return StoreAPI.get(Endpoints.FIND_ORDER.format(order_id))
 
     @staticmethod
-    def delete_order(order_id: int):
-        return requests.delete(Endpoints.DELETE_ORDER.format(order_id))
+    def delete_order(order_id):
+        return StoreAPI.delete(Endpoints.DELETE_ORDER.format(order_id))
 
     @staticmethod
     def get_inventory():
-        return requests.get(Endpoints.GET_INVENTORY)
+        return StoreAPI.get(Endpoints.GET_INVENTORY)
